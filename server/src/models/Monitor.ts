bd5e1ba790e38e07bd9a49eedf88fd7621a488d0
Lewis <lewis.burgess314@gmail.com>
2025-04-24 12:33:05 +0100

    Adds the monitor model

---
+++ server/src/models/Monitor.ts
@@ -0,0 +1,28 @@
+import mongoose, { Schema, model, InferSchemaType, HydratedDocument} from 'mongoose'
+
+const monitorSchema = new Schema({
+  user: {
+    type: mongoose.SchemaTypes.ObjectId,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
+  },
+  interval: {
+    type: Number,
+    default: 5
+  },
+  lastStatus: {
+    type: String,
+    enum: ['UP', 'DOWN', 'NOTCHECKED'],
+    default: 'NOTCHECKED'
+  },
+  lastChecked: Date,
+  createdAt: Date,
+})
+
+export type MonitorType = InferSchemaType<typeof monitorSchema>
+export type MonitorDocument = HydratedDocument<MonitorType>
+
+export default model<MonitorType>('Monitor', monitorSchema)
