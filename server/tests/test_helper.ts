223e871b0937ecbb962a7f944db343084f0d60e1
Lewis <lewis.burgess314@gmail.com>
2025-04-24 09:49:03 +0100

    Adds test for user registration route

--- server/tests/test_helper.ts
+++ server/tests/test_helper.ts
@@ -1,19 +1,40 @@
-import mongoose from "mongoose"
 import User from "../src/models/User.js"
+import bcrypt from "bcryptjs"
 
-const initialUsers: {email: string, password: string}[] = [
+// Password hash for initial users
+const passwordHash = await bcrypt.hash('SuperStrong1!', 8)
+
+// Initial users to add to the database
+const initialUsers: {email: string, passwordHash: string}[] = [
   {
     email: 'firstUser@email.com',
-    password: 'SuperStrong1!'
+    passwordHash
   },
   {
     email: 'secondUser@email.com',
-    password: 'SuperStrong1'
+    passwordHash
   }
 ]
 
+// User info for adding a user
+const userToAdd = {
+  email: 'userToAdd@email.com',
+  password: 'SuperStrong1!'
+}
+
+// For clearing the user data from the database
 const clearUserData = async () => {
   await User.deleteMany({})
 }
 
-export default { clearUserData, initialUsers } 
+// For retrieving the users from the database
+const usersInDb = async () => {
+  return await User.find({})
+}
+
+export default { 
+  clearUserData,
+  initialUsers,
+  userToAdd,
+  usersInDb
+ } 
--- server/tests/users_api.test.ts
+++ server/tests/users_api.test.ts
@@ -1,22 +1,56 @@
-import {test, after, beforeEach} from 'node:test'
+import {test, after, beforeEach, describe} from 'node:test'
+import assert from 'node:assert'
 import app from '../src/app.js'
 import supertest from 'supertest'
 import mongoose from 'mongoose'
 import helper from './test_helper.js'
+import User from '../src/models/User.js'
+
 
 const api = supertest(app)
-
-// Initialises the database before running tests
-beforeEach(async () => {
-  await helper.clearUserData()
-  const firstUser = 
-})
 
 test('app is healthy', async () => {
   await api
     .get('/health')
     .expect(200)
     .expect('Content-Type', /text\/html/)
+})
+
+
+
+describe('When there are initially some users in the database', () => {
+  // Initialises the database with users before each run
+  beforeEach(async () => {
+    console.log('database ~~~~~')
+    await helper.clearUserData()
+    await User.insertMany(helper.initialUsers)
+  })
+
+  describe('adding a new user', () => {
+
+    test('succeeds with valid data', async () => {
+      // Registers the new user
+      await api.post('/api/users/register')
+        .send(helper.userToAdd)
+        .expect(201)
+      
+      // Asserts that the user count has increased
+      const usersInDb = await helper.usersInDb()
+      assert.strictEqual(usersInDb.length, helper.initialUsers.length + 1)
+    })
+
+    test('fails with existing user data', async () => {
+      // Attempts to register with existing data
+      await api.post('/api/users/register')
+        .send(helper.initialUsers[0])
+        .expect(400)
+      
+      // Asserts that the user count has not increased
+      const usersInDb = await helper.usersInDb()
+      assert.strictEqual(usersInDb.length, helper.initialUsers.length)
+    })
+
+  })
 })
 
 after(async () => {
