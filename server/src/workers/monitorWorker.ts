f344e512abb45f1dd54671dc8ef0f2646540e3fc
Lewis <lewis.burgess314@gmail.com>
2025-04-30 12:28:37 +0100

    Refactored monitor worker to start worker in the file its defined in

--- server/src/workers/monitorWorker.ts
+++ server/src/workers/monitorWorker.ts
@@ -5,10 +5,15 @@ import { monitorQueueName } from "../queue/monitorQueue.js"
 import { checkUrlStatus, updateMonitorStatusAndReturnOldIfChanged } from "./utils/helpers.js"
 import logger from "../utils/logger.js"
 import { addDiscordStatusChangeNotificationJob } from "../queue/notifyQueue.js"
+import mongoose from "mongoose"
+
+console.log('connecting monitor worker to mongoDB...')
+await mongoose.connect(config.MONGODB_URL)
+console.log('connected monitor worker to mongoDB')
 
 const connection = new Redis(config.UPSTASH_ENDPOINT, {maxRetriesPerRequest: null})
 
-export default new Worker(monitorQueueName, async (job) => {
+const monitorWorker = new Worker(monitorQueueName, async (job) => {
   try {
     // Checks the status of the url
     const urlCheckResult = await checkUrlStatus(job.data.url)
@@ -17,6 +22,7 @@ export default new Worker(monitorQueueName, async (job) => {
     
     // If the status changes, adds the discord notification job to the queue
     if (hasChanged){
+      console.log('monitor worker added a noitification job to the queueu')
       await addDiscordStatusChangeNotificationJob({
         webHookUrl: config.TEMP_DISCORD_WEBHOOK,
         url: job.data.url,
@@ -26,6 +32,12 @@ export default new Worker(monitorQueueName, async (job) => {
     }
   } catch (error) {
     logger.error('error checking status of url', error)
+    throw error
   }
 
 }, {autorun: false, connection})
+
+
+
+monitorWorker.run()
+console.log('started monitor worker')
