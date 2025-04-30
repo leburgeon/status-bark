970cbc3e33a776832d978f18a9df76f8fb52641f
Lewis <lewis.burgess314@gmail.com>
2025-04-30 11:47:28 +0100

    Added a notifyer that uses discord webhooks to notify of status change

---
+++ server/src/workers/utils/discordNotifyer.ts
@@ -0,0 +1,10 @@
+import axios from 'axios'
+
+interface urlStatus {status: string, timeChecked: Date}
+
+export const notifyDiscordOfStatusChange = async (url: string, oldStatus: urlStatus, newStatus: urlStatus, webHookUrl: string) => {
+  await axios.post(webHookUrl, {
+    username: 'Status-Bark-Bot',
+    content: `URL: ${url} was ${oldStatus.status} when last checked at ${oldStatus.timeChecked} but its status has changed to ${newStatus.status}! (checked at: ${newStatus.timeChecked})`
+  })
+}
