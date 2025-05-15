9c181aed13b138d4cd85c47d81b62b7a7df25402
Lewis <lewis.burgess314@gmail.com>
2025-05-15 14:32:42 +0100

    Initial implementation of cryptography helper functions

--- server/src/utils/helper.ts
+++ server/src/utils/helper.ts
@@ -1,12 +1,59 @@
 import crypto from 'crypto'
 import config from './config.js'
 
-export const encryptDiscordWebhook = (webhook: string) => {
-  console.log('boop beep encrypting ' + webhook)
-  return crypto.randomBytes(32).toString('hex')
+// For combining the encrypted cipher elements into a payload
+const combineEncryptedPayload = (iv: Buffer, tag: Buffer, ciphertext: Buffer) => {
+  const payload = Buffer.concat([iv, tag, ciphertext]).toString('base64')
+  return payload
 }
 
-export const decryptDiscordWebhook = (encryptedWebhook: string) => {
-  console.log('beep boop decrypting '+ encryptedWebhook)
-  return config.TEST_DISCORD_WEBHOOK
+// For seperating the encrypted payload into the buffer elements
+const seperateEncryptedPayload = (payload: string): {iv: Buffer, tag: Buffer, ciphertext: Buffer} => {
+  const encryptedBuffer = Buffer.from(payload, 'base64')
+
+  const iv = encryptedBuffer.subarray(0, 12)
+  const tag = encryptedBuffer.subarray(12, 28)
+  const ciphertext = encryptedBuffer.subarray(28)
+
+  return {iv, tag, ciphertext}
+}
+
+export const encryptSymmetricIntoPayload = (webhook: string): string => {
+  // Creates the iv to be at the start of the encryption blocks, ensuring uniqueness
+  const iv = crypto.randomBytes(12)
+
+  // Creates the cipher, using the key and the iv
+  const cipher = crypto.createCipheriv('aes-256-gcm', Buffer.from(config.WEBHOOK_ENCRYPTION_KEY, 'base64'), iv)
+
+  // Updates the created cipher with the webhook to encrypt and the encoding
+  let ciphertext = cipher.update(webhook, 'utf-8', 'base64')
+
+  // Finalises the encryption
+  ciphertext += cipher.final('base64')
+
+  // Gets the auth tag for the encryption
+  const tag = cipher.getAuthTag()
+
+  // Combines and returns the payload
+  const payload = combineEncryptedPayload(iv, tag, Buffer.from(ciphertext, 'base64'))
+  return payload
+}
+
+export const decryptDiscordWebhookPayload = (encryptedPayload: string): string => {
+  // Seperates the elements for decryption from the payload
+  const {iv, tag, ciphertext} = seperateEncryptedPayload(encryptedPayload)
+
+  // Creates the decypher object using the key and the initialisation vector
+  const decipher = crypto.createDecipheriv('aes-256-gcm', Buffer.from(config.WEBHOOK_ENCRYPTION_KEY, 'base64'), iv)
+
+  // Sets the authorisation tag
+  decipher.setAuthTag(tag)
+
+  // Decrypts the ciphertext
+  let plaintext = decipher.update(ciphertext.toString('base64'), 'base64', 'utf-8')
+
+  // Finalises the decryption
+  plaintext += decipher.final('base64')
+
+  return plaintext
 }
