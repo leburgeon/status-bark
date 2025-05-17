b59dac576f7e6699e1f9f2afd2faf51bc0288809
Lewis <lewis.burgess314@gmail.com>
2025-05-17 11:08:37 +0100

    Adds the store provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,14 +4,21 @@ import App from './App.tsx'
 import {theme} from './assets/theme.ts'
 import { ThemeProvider } from '@mui/material'
 import {CssBaseline} from '@mui/material'
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
+import store from './store.ts'
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline>
-        <App />
-      </CssBaseline>
-    </ThemeProvider>
+    <Provider store={store}>
+      <Router>
+        <ThemeProvider theme={theme}>
+          <CssBaseline>
+            <App />
+          </CssBaseline>
+        </ThemeProvider>
+      </Router>
+    </Provider>
   </StrictMode>,
 )
