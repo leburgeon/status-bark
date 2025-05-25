fbd886ea030388f431f7f264cf8e94f7200f4215
Lewis <lewis.burgess314@gmail.com>
2025-05-25 13:06:12 +0100

    Monitor slice progress

--- client/src/reducers/monitorsSlice.ts
+++ client/src/reducers/monitorsSlice.ts
@@ -1,14 +1,47 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { z } from "zod";
 
-const MonitorSchema = z.object({
+export const MonitorSchema = z.object({
   id: z.string(),
   nickname: z.string(),
   url: z.string(),
-  status: z.string()
-  
+  lastStatus: z.string(),
+  createdAt: z.string(),
+  discordWebhook: z.object({
+    notify: z.boolean(),
+    urlPresent: z.boolean()
+  })
 })
 
+type Monitor = z.infer<typeof MonitorSchema>
+
+const initialState: {monitors: Monitor[]} = {monitors: []}
+
 const monitorSlice = createSlice({
-  name: 'monitors'
+  name: 'monitors',
+  initialState,
+  reducers: {
+    setMonitors: (state, action: PayloadAction<Monitor[]>) => {
+      state.monitors = action.payload
+    },
+    clearMonitors: (state) => {
+      state.monitors = []
+    },
+    updateMonitor: (state, action: PayloadAction<Monitor>) => {
+      state.monitors = state.monitors.map(monitor => {
+        if (monitor.id !== action.payload.id){
+          return monitor
+        } else {
+          return action.payload
+        }
+      })
+    },
+    deleteMonitor: (state, action: PayloadAction<string>) => {
+      state.monitors = state.monitors.filter(monitor => monitor.id !== action.payload)
+    }
+  }
 })
+
+export const {setMonitors, clearMonitors} = monitorSlice.actions
+
+export default monitorSlice.reducer
